3660cac01d8c15960a709e3c31d8f50d8b6d7bf8
Bhunesh ojha108 <149822963+Bhuneshojha@users.noreply.github.com>
2024-04-02 10:45:48 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,26 @@
+//          100 days of coding challenge ( DAY 19 )
+
+//   Question 01   
+  //---Starts with a list of numbers
+let numbers = [1, 2, 3, 4, 5];
+
+  // ---Doubles each number
+let doubledNumbers = numbers.map(number => number * 2);
+
+  // ---Shows the new list of doubled numbers
+console.log(doubledNumbers); 
+//   Question 02    
+let mixedArray = [1,"bharti",4,"sourav",true,"rishi"];
+  // -- shows string array
+let stringArray = mixedArray.filter(items => typeof items === "string")
+
+console.log(stringArray);
+  // -- shows numbers array
+let numberArray = mixedArray.filter(items => typeof items === "number")
+console.log(numberArray);
+//   Question 03 
+let grades = [87, 91, 73, 75, 65, 52]
+ // ---calculate the average grade
+let averageGrade = grades.reduce((total, grade) => total + grade, 0) / grades.length
+ // ---shows averages grade
+console.log(averageGrade);
